651eb46e3d78b104d1f9ddc634aaec57d7dbc139
3stbn <esteban93torres@gmail.com>
2020-06-04 14:46:26 -0500

    update social links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -25,36 +25,39 @@
 <footer>
   <div class="container">
     <div class="columns social-links has-text-link">
-      <div class="column social-link ">
+      <a class="column social-link " href="https://github.com/3stbn">
         <span class="icon is-size-1">
           <i class="fab fa-github" />
         </span>
         <span class=" username">@3stn</span>
-      </div>
-      <div class="column social-link">
+      </a>
+      <a class="column social-link" href="https://twitter.com/stbantorres">
         <span class="icon is-size-1">
           <i class="fab fa-twitter" />
         </span>
         <span class=" username">@stbantorres</span>
-      </div>
-      <div class="column social-link">
+      </a>
+      <a
+        class="column social-link"
+        href="https://www.youtube.com/channel/UCbWV65OoGP6mw2pGd0c5E-g">
         <span class="icon is-size-1">
           <i class="fab fa-youtube" />
         </span>
         <span class=" username">Esteban Codes</span>
-      </div>
-      <div class="column social-link">
+      </a>
+      <a class="column social-link" href="mailto:hi@esteban.site">
         <span class="icon is-size-1">
           <i class="fas fa-envelope" />
         </span>
         <span class=" username">hi@esteban.site</span>
-      </div>
+      </a>
     </div>
   </div>
   <div class="copy">
     <div class="container has-text-centered has-text-link">
       ©{new Date().getFullYear()} Esteban Torres, all rights reserved. Made with
-      ❤️ with sapper
+      ❤️ with
+      <a href="https://sapper.svelte.dev/">Sapper</a>
     </div>
   </div>
 </footer>
